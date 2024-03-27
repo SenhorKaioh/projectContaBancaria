bc5b1d0509f267b30d60bdd0229cce658b7751f1
Caio <caiopsantos.dev@gmail.com>
2024-03-27 00:42:55 -0300

    Commit do Menu

---
+++ Menu.ts
@@ -0,0 +1,111 @@
+import readlinesync = require("readline-sync");
+import { colors } from './Cores';
+
+export function main(){
+
+let cod: number;
+
+
+
+while(true) {
+
+console.log(colors.bg.blue, colors.fg.yellow,
+            "                                                  ")
+console.log("|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*|")
+console.log("|     SEJA BEM-VINDO AO BANCO CARIOCAS           -|")
+console.log("|                                                -|")
+console.log("|                                                -|")
+console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|")
+console.log("|                                                -|")
+console.log("|                                                -|")
+console.log("|        1 - Criar Conta                         -|")
+console.log("|        2 - Listar todas as contas              -|")
+console.log("|        3 - Buscar Conta por Numero             -|")
+console.log("|        4 - Atualizar Dados da conta            -|")
+console.log("|        5 - Apagar a Conta                      -|")
+console.log("|        6 - Sacar                               -|")
+console.log("|        7 - Depositar                           -|")
+console.log("|        8 - Transferir valores entre Contas     -|")
+console.log("|        9 - Sair                                -|")
+console.log("|                                                -|")
+console.log("|                                                -|")
+console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|")
+console.log("                                                   ",colors.reset);
+
+console.log("Entre com a opção desejada: ");
+cod = readlinesync.questionInt("");
+
+if (cod === 9){
+    console.log("\nBANCO CARIOCAS - SUA VIDA FINANCEIRO MELHOR TODO DIA!");
+    sobre();
+    process.exit(0);
+}
+switch(cod){
+    case 1:
+                console.log(colors.fg.magenta,
+                    "\n\nCriar Conta\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.magenta,
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 3:
+                console.log(colors.fg.magenta,
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 4:
+                console.log(colors.fg.magenta,
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 5:
+                console.log(colors.fg.magenta,
+                    "\n\nApagar uma Conta\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 6:
+                console.log(colors.fg.magenta,
+                    "\n\nSaque\n\n", colors.reset);
+                    keyPress()
+                break;
+            case 7:
+                console.log(colors.fg.magenta,
+                    "\n\nDepósito\n\n", colors.reset);
+                    keyPress()
+
+                break;
+            case 8:
+                console.log(colors.fg.magenta,
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
+                    keyPress()
+
+                break;
+            default:
+                console.log(colors.fg.magenta,
+                    "\nOpção Inválida!\n", colors.reset);
+                    keyPress()
+                break;
+        }
+    }
+
+}
+
+/* Função com os dados da pessoa desenvolvedora */
+
+export function sobre(): void {
+    console.log("\n*****************************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Caio Pereira - Caiopsantos.dev@gmail.com");
+    console.log("github.com/SenhorKaioh");
+    console.log("*****************************************************");
+}
+function keyPress(): void{
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+main();
---
+++ model/Conta.ts
@@ -0,0 +1,113 @@
+
+
+export class Conta{
+
+    private _numero: number;
+    private _agencia: number;
+    private _tipo: number;
+    private _titular: string;
+    private _saldo: number;
+
+	constructor(numero: number, agencia: number, tipo: number, titular: string, saldo: number) {
+		this._numero = numero;
+		this._agencia = agencia;
+		this._tipo = tipo;
+		this._titular = titular;
+		this._saldo = saldo;
+	}
+
+    /**
+     * Getter numero
+     * @return {number}
+     */
+	public get numero(): number {
+		return this._numero;
+	}
+
+    /**
+     * Getter agencia
+     * @return {number}
+     */
+	public get agencia(): number {
+		return this._agencia;
+	}
+
+    /**
+     * Getter tipo
+     * @return {number}
+     */
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+    /**
+     * Getter titular
+     * @return {string}
+     */
+	public get titular(): string {
+		return this._titular;
+	}
+
+    /**
+     * Getter saldo
+     * @return {number}
+     */
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+    /**
+     * Setter numero
+     * @param {number} value
+     */
+	public set numero(value: number) {
+		this._numero = value;
+	}
+
+    /**
+     * Setter agencia
+     * @param {number} value
+     */
+	public set agencia(value: number) {
+		this._agencia = value;
+	}
+
+    /**
+     * Setter tipo
+     * @param {number} value
+     */
+	public set tipo(value: number) {
+		this._tipo = value;
+	}
+
+    /**
+     * Setter titular
+     * @param {string} value
+     */
+	public set titular(value: string) {
+		this._titular = value;
+	}
+
+    /**
+     * Setter saldo
+     * @param {number} value
+     */
+	public set saldo(value: number) {
+		this._saldo = value;
+	}
+
+public visualizar(): void{
+    console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|");
+    console.log("|               Dados da Conta                   -|");
+    console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|");
+    console.log(`Número da Conta: ${this._numero}`);
+    console.log(`Número da agência: ${this._agencia}`);
+    console.log(`Tipo da Conta: ${this._tipo}`);
+    console.log(`Titular da Conta: ${this._titular}`);
+    console.log(`Saldo da Conta: ${this._saldo}`);
+    console.log("|                                                -|");
+    console.log("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|");
+}
+
+}
+
